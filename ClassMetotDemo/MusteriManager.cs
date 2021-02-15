08116f96a1e85f33a7384d2da5ea260ac37d27bf
Cemre Su ANT <cemre.ant@metu.edu.tr>
2021-02-16 01:06:11 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri) 
+        {
+            Console.WriteLine(musteri.Id + " No'lu Müşteri Eklendi" );
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine( musteri.AdSoyad+ " No'lu Müşteri Listelendi");
+        }
+
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.HesapNo + " No'lu Müşteri Silindi");
+        }
+    }
+    
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.AdSoyad = "Cemre Ant";
+            musteri1.HesapNo = " 1212121212 ";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.AdSoyad = "Levent Ant";
+            musteri2.HesapNo = "898989898";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.AdSoyad = "Tülin Ant";
+            musteri3.HesapNo = "56565656";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Silme(musteri1);
+            musteriManager.Listele(musteri1);
+
+
+
+
+        }
+    }
+}
